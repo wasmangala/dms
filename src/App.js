d39c0a71293535bd99f2e5ea70b4e401eabed0bf
wasmangala <wasmangala@gmail.com>
2017-06-05 19:21:09 +0530

    login created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import './login.css';
 
 
 
@@ -89,18 +90,42 @@ class App extends Component {
 </nav>
 
 
+<div id="loginbox" className="mainbox col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3"> 
+
+<div className="panel panel-default" >
+            <div className="panel-heading">
+                <div className="panel-title text-center">Please Login</div>
+            </div>     
+
+            <div className="panel-body" >
+
+                <form name="form" id="form" className="form-horizontal" enctype="multipart/form-data" method="POST">
+                   
+                    <div className="input-group">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                        <input id="user" type="text" className="form-control" name="user" value="" placeholder="Username"/>                                        
+                    </div>
+
+                    <div className="input-group">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i></span>
+                        <input id="password" type="password" className="form-control" name="password" placeholder="Password"/>
+                    </div>                                                                  
+
+                    <div className="form-group">
+                    
+                        <div className="col-sm-12 controls">
+                            <button type="submit" href="#" className="btn btn-primary pull-right"><i className="glyphicon glyphicon-log-in"></i> Log in</button>                          
+                        </div>
+                    </div>
+
+                </form>     
+
+            </div>                     
+        </div>
 
 
-<div className="jumbotron">
-  <h1 className="text-center">Welcome!</h1>
-  <p className="text-center">to</p>
-  <h2 className="text-center">DMS Cadre Management System </h2>
-
-  <p className="text-center"><a className="btn btn-primary btn-sm" href="#" role="button">Login</a></p>
-</div>
-
-
-
+      </div>
+  
 
 </div>
     );
